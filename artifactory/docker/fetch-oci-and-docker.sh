82d0c8f20ae22da98d06ababf5166f048a5bace9
John Osborne <josborne@chainguard.dev>
2026-06-11 11:28:24 -0400

    Update fetch-oci-and-docker.sh
    Signed-off-by: John Osborne <josborne@chainguard.dev>

--- artifactory/docker/fetch-oci-and-docker.sh
+++ artifactory/docker/fetch-oci-and-docker.sh
@@ -11,6 +11,8 @@ output_file=${1:-repositories.txt}
 # Step 1: Fetch repository names
 reponames=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: text/plain" "$artifactory_url/api/search/aql" --data 'items.find().include("repo")' | jq -r '.results[].repo')
 # reponames=$(curl -s -X POST -H "Authorization: Bearer $token" -H "Content-Type: text/plain" "$artifactory_url/api/search/aql" --data 'items.find({"repo": "cgr-pov", "type": "file", "name": {"$match": "manifest.json"}}).include("repo", "path", "name")' | jq -r '.results[] | "\(.repo)/\(.path)"')
+# curl -s -H "Authorization: Bearer $token" "$artifactory_url/artifactory/api/docker/cgr-pov/v2/_catalog" | jq .
+# curl -s -H "Authorization: Bearer $token" "$artifactory_url/artifactory/api/docker/cgr-pov/v2/go/tags/list" | jq .
 
 echo ""
 echo "The following repo names have been save to $output_file:"
