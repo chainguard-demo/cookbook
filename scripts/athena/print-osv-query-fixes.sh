831e5dda0ab656a48da685135985185a226b7d8e
John Osborne <josborne@chainguard.dev>
2026-07-07 14:27:21 -0400

    Update print-osv-query-fixes.sh
    Signed-off-by: John Osborne <josborne@chainguard.dev>

--- scripts/athena/print-osv-query-fixes.sh
+++ scripts/athena/print-osv-query-fixes.sh
@@ -3,9 +3,20 @@
 echo "Fetching gavs endpoint"
 curl -L --user "$CHAINGUARD_JAVA_IDENTITY_ID:$CHAINGUARD_JAVA_TOKEN" "$BASEURL-/api/gavs?page_size=1000" 2>/dev/null | jq > gavs.json
 echo "GAVs found: $(jq '.gavs | length' gavs.json)"
-echo ".cgr fixes: $(jq '[.gavs[] | capture("cgr\\.(?<n>[0-9]+)") | .n | tonumber] | add // 0' gavs.json)"
-echo ".cgp fixes: $(jq '[.gavs[] | capture("cgp\\.(?<n>[0-9]+)") | .n | tonumber] | add // 0' gavs.json)"
 echo "saving results to gavs.json"
+
+echo ".cgp fixes: $(jq '
+  [.gavs[] | . as $gav
+    | capture("\\.cgp\\.(?<n>[0-9]+)") as $m
+    | {
+        base: ($gav | sub("\\.cgp\\.[0-9]+"; "")),
+        n: ($m.n | tonumber)
+      }
+  ]
+  | group_by(.base)
+  | map(map(.n) | max)
+  | add // 0
+' gavs.json)"
 
 mkdir -p jars patches
 for gav in $(jq -r '.gavs[]' gavs.json); do
