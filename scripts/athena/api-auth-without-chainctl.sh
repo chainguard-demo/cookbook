268ff83cf2eeda80d3aa393f2af377a79c94be92
John Osborne <josborne@chainguard.dev>
2026-07-14 23:57:25 -0400

    Enhance api-auth-without-chainctl.sh with comments
    Added comments to explain the purpose of the script.
    
    Signed-off-by: John Osborne <josborne@chainguard.dev>

---
+++ scripts/athena/api-auth-without-chainctl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# In this example I login to the Chainguard Console and using the OIDC integrations (Google in my case) and then use that OIDC token to programmatically make API calls
+export CONSOLE_API_URL_QUERY="https://console-api.enforce.dev/argos/v1/osv/query"
+export AUDIENCE="https://console-api.enforce.dev"
+export IDENTITY="a79ad76794eb3869959effe72929836b37b34ecb"
+export PORT=8989
+
+printf 'HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\nDone' | nc -l "$PORT" > /tmp/cg_callback &
+xdg-open "https://issuer.enforce.dev/oauth?audience=https://console-api.enforce.dev&client_id=auth0&exit=redirect&skip_registration=true&redirect=http%3A%2F%2Flocalhost%3A${PORT}%2Fcallback%3Ftoken%3Dtrue"
+sleep 5
+export IDENTITY_TOKEN=$(grep -o 'token=[^& ]*' /tmp/cg_callback | grep -v 'token=true' | head -1 | cut -d= -f2)
+echo "$IDENTITY_TOKEN"
+export name="com.amazon.ion:ion-java"
+echo "name=$name"
+curl -s "$CONSOLE_API_URL_QUERY" \
+       -H "Authorization: Bearer $IDENTITY_TOKEN" \
+       -H 'Content-Type: application/json' \
+       -d "{\"package\":{\"ecosystem\":\"Maven\",\"name\":\"${name}\"}}" | jq
